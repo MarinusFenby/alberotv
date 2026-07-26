d3ad2f471f9bfbe18f96b13f0ff148854e62dc08
MarinusFenby <formal.cia@live.com>
2026-07-26 21:19:41 +0400

    Add alert for script load confirmation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert("SCRIPT CARGADO");
 const timeline = document.getElementById("timeline");
 const hint = document.getElementById("hint");
 const categoryList = document.getElementById("category-list");
